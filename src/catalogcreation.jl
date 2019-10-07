7e79d52bc1b96ceb45c7b9b05075ccfc5d92a743
Forrest Gasdia <EP-Guy@users.noreply.github.com>
2019-10-06 23:24:19 -0600

    Break out code for original star catalog reading

---
+++ src/catalogcreation.jl
@@ -0,0 +1,9 @@
+
+f = CSV.file("URAT1-10mag.csv")
+
+catalog = Array{CatalogStar}(undef, f.rows)
+for (i, s) in enumerate(f)
+  a, b = split(s.URAT1, '-')
+  starid = parse(Int, a*b)
+  catalog[i] = CatalogStar(starid, s.RAJ2000, s.DEJ2000, s.f_mag)
+end
