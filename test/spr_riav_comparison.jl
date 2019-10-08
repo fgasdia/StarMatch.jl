7370f63265607bedaa0fc7c29db0d8483fb29c33
Forrest Gasdia <EP-Guy@users.noreply.github.com>
2019-10-07 20:44:17 -0600

    Test code comparing to Samirbhai

--- test/spr_riav_comparison.jl
+++ test/spr_riav_comparison.jl
@@ -1,12 +1,104 @@
+using Test
+using CSV
+using StaticArrays
+
 using StarMatch
 
-camera = Camera(1024, 1024, 13.3e-6, 0.0, 15.)
+
+img_width = 1024
+img_height = 1024
+pixelsize = 13.3e-6
+fov = 15.0
+fl = img_height*pixelsize/2/tand(fov/2)
+camera = StarMatch.Camera(img_width, img_height, pixelsize, fl, fov)
 
 f = CSV.file("test\\SKY2000_Magnitude6_doublestars_0.12.txt")
 
-catalog = Array{CatalogStar}(undef, f.rows)
+catalog = Array{StarMatch.CatalogStar}(undef, f.rows)
 for (i, s) in enumerate(f)
-  catalog[i] = CatalogStar(s.ID, s.RAJ2000, s.DEJ2000, s.mag)
+    catalog[i] = StarMatch.CatalogStar(s.ID, s.RAJ2000, s.DEJ2000, s.mag)
 end
 
-spd = generatespd(camera, catalog)
+function readspdfile(f::IOStream)
+    vectorpattern = []
+    for line in eachline(f)
+        numstrs = split(line, "\t")
+
+        nums = Int[]
+        for str in numstrs
+            if str != ""
+                push!(nums, parse(Int, str))
+            end
+        end
+
+        push!(vectorpattern, SVector.(nums[1:2:end], nums[2:2:end]))
+    end
+
+    return vectorpattern
+end
+
+@testset "SPD creation" begin
+    spd = StarMatch.generatespd(camera, catalog)
+
+    d1 = open(readspdfile, "test\\SPD_vect_patt_Mv_6_dist_1.txt")
+    d2 = open(readspdfile, "test\\SPD_vect_patt_Mv_6_dist_2.txt")
+    d3 = open(readspdfile, "test\\SPD_vect_patt_Mv_6_dist_3.txt")
+    d4 = open(readspdfile, "test\\SPD_vect_patt_Mv_6_dist_4.txt")
+
+    # Choose a couple random stars as center star (`starO`) and then compare
+    # paths for each `SP` star
+
+    # starid = 14
+    spd_14 = findall(x->x.Oid == 14, spd)
+
+    d1_diff = d1[14] - spd[spd_14[1]].path
+    d1_diffr = [d[1] for d in d1_diff]
+    d1_diffc = [d[2] for d in d1_diff]
+    @test all(abs.(d1_diffc) .< 1)
+    @test all(abs.(d1_diffr) .< 1)
+
+    d2_diff = d2[14] - spd[spd_14[2]].path
+    d2_diffr = [d[1] for d in d2_diff]
+    d2_diffc = [d[2] for d in d2_diff]
+    @test all(abs.(d2_diffc) .< 1)
+    @test all(abs.(d2_diffr) .< 1)
+
+    d3_diff = d3[14] - spd[spd_14[3]].path
+    d3_diffr = [d[1] for d in d3_diff]
+    d3_diffc = [d[2] for d in d3_diff]
+    @test all(abs.(d3_diffc) .< 1)
+    @test all(abs.(d3_diffr) .< 1)
+
+    d4_diff = d4[14] - spd[spd_14[4]].path
+    d4_diffr = [d[1] for d in d4_diff]
+    d4_diffc = [d[2] for d in d4_diff]
+    @test all(abs.(d4_diffc) .< 1)
+    @test all(abs.(d4_diffr) .< 1)
+
+    # starid = 3864
+    spd_3864 = findall(x->x.Oid == 3864, spd)
+
+    d1_diff = d1[3864] - spd[spd_3864[1]].path
+    d1_diffr = [d[1] for d in d1_diff]
+    d1_diffc = [d[2] for d in d1_diff]
+    @test all(abs.(d1_diffc) .< 1)
+    @test all(abs.(d1_diffr) .< 1)
+
+    d2_diff = d2[3864] - spd[spd_3864[2]].path
+    d2_diffr = [d[1] for d in d2_diff]
+    d2_diffc = [d[2] for d in d2_diff]
+    @test all(abs.(d2_diffc) .< 1)
+    @test all(abs.(d2_diffr) .< 1)
+
+    d3_diff = d3[3864] - spd[spd_3864[3]].path
+    d3_diffr = [d[1] for d in d3_diff]
+    d3_diffc = [d[2] for d in d3_diff]
+    @test all(abs.(d3_diffc) .< 1)
+    @test all(abs.(d3_diffr) .< 1)
+
+    d4_diff = d4[3864] - spd[spd_3864[4]].path
+    d4_diffr = [d[1] for d in d4_diff]
+    d4_diffc = [d[2] for d in d4_diff]
+    @test all(abs.(d4_diffc) .< 1)
+    @test all(abs.(d4_diffr) .< 1)
+end
