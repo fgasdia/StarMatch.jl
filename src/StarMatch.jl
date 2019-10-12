345ed7602daf105f08c298cf25a9a099bc317b35
Forrest Gasdia <EP-Guy@users.noreply.github.com>
2019-10-11 20:34:58 -0600

    Calculate radec2eci once for each catalog star in generatespd
    radec2eci profiled as about 50% of the cycles for generatespd, probably due to the 4 trig calls. This also replaces 4 sind or cosd calls with two sincos calls

--- src/StarMatch.jl
+++ src/StarMatch.jl
@@ -30,9 +30,7 @@ struct UnknownStar
 end
 
 struct KnownStar
-    id::Int
-    ra::Float64
-    dec::Float64
+    catalog::CatalogStar
     xy::SVector{2, Float64}
     vec::SVector{2, Float64}
     uvec::SVector{2, Float64}
@@ -41,9 +39,10 @@ end
 
 struct SPDEntry
     starO::CatalogStar
-    SPid::Int
+    SPid::CatalogStar
     d::Float64
     path::Array{SVector{2, Float64}}
+    pathstars::Array{CatalogStar}
 end
 
 struct Camera
@@ -67,8 +66,9 @@ RA and DEC in degrees.
 See Vallado 4-1.
 """
 function radec2eci(ra, dec)
-    Cδ = cosd(dec)
-    return SVector(cosd(ra)*Cδ, sind(ra)*Cδ, sind(dec))
+    sinδ, cosδ = sincos(deg2rad(dec))
+    sinα, cosα = sincos(deg2rad(ra))
+    return SVector(cosα*cosδ, sinα*cosδ, sinδ)
 end
 
 """
@@ -152,17 +152,22 @@ function generatespd(camera::Camera, catalog::Array{CatalogStar})
     halffov = camera.fov/2
     coshalffov = cosd(halffov)
 
+    # Calculating star ECI is surprisingly expensive, so we do it once
+    catalogeci = [radec2eci(s.ra, s.dec) for s in catalog]
+
     spd = SPDEntry[]
-    @showprogress 5 "Building SPD..." for starO in catalog
+    sizehint!(spd, trunc(Int, 0.9*length(catalog)))
+    # @showprogress 5 "Building SPD..." for starO in catalog
+    for (oi, starO) in enumerate(catalog)
         # Transform from RA/DEC to ECI XYZ
-        VeciO = radec2eci(starO.ra, starO.dec)
+        VeciO = catalogeci[oi]
         CO = cameraattitude(VeciO)
         xyO = camera2image(CO*VeciO, camera)  # should be the center pixel
 
         # Identify neighboring stars in FOV
         neighbors = KnownStar[]
-        @inbounds for s in catalog
-            Veci = radec2eci(s.ra, s.dec)
+        @inbounds for (si, s) in enumerate(catalog)
+            Veci = catalogeci[si]
 
             # Is star `s` within fov/2 of `starO`?
             # based on dot product / cosine similarity
@@ -182,7 +187,7 @@ function generatespd(camera::Camera, catalog::Array{CatalogStar})
                 # Unit vector from `starO` to star `s`
                 usvec = svec/d
 
-                push!(neighbors, KnownStar(s.id, s.ra, s.dec, xy, svec, usvec, d))
+                push!(neighbors, KnownStar(s, xy, svec, usvec, d))
             end
         end
 
@@ -208,20 +213,18 @@ function generatespd(camera::Camera, catalog::Array{CatalogStar})
             # Generate path for current SP
             sn = @view neighbors[i:end]
             path = buildpath(sn, V1, V1u)
+            pathstars = [n.catalog for n in sn]
 
-            push!(spd, SPDEntry(starO, neighbors[i].id, neighbors[i].d, path))
+            push!(spd, SPDEntry(starO, neighbors[i].catalog, neighbors[i].d, path, pathstars))
         end
     end
     return spd
 end
 
-function solve(
-    camera::Camera,
-    imagestars::AbstractArray{SVector{2, T}},
-    spd::AbstractArray{SPDEntry};
-    distancetolerance=1) where T <: Real
+function solve(camera::Camera, imagestars::AbstractArray{SVector{2, T}},
+    spd::AbstractArray{SPDEntry}; distancetolerance=1) where T <: Real
 
-    @assert length(imagestars) > N_NEAREST "A minimum of 5 stars are required to solve image."
+    @assert length(imagestars) > N_NEAREST "A minimum of 5 stars required to solve image."
 
     imagecenter = SVector(camera.img_height/2, camera.img_width/2)
 
@@ -284,128 +287,8 @@ function solve(
     winnervotecount > VOTE_THRESHOLD && println("Not enough votes!")
 
     winner = spd[candidateindices[winnerid][2]]
+
+    return winner
 end
-
-
-
-
-
-# f = FITS("test\\axy.fits")
-#
-# X = read(f[2], "X")
-# Y = read(f[2], "Y")
-#
-# imagestars = [SVector(x, y) for (x, y) in zip(X, Y)]
-#
-#
-# # RASA/Manta
-# camera = Camera(1936, 1216, 5.86e-6, 620e-3)
-#
-# f = CSV.file("URAT1-10mag.csv")
-#
-# catalog = Array{CatalogStar}(undef, f.rows)
-# for (i, s) in enumerate(f)
-#   a, b = split(s.URAT1, '-')
-#   starid = parse(Int, a*b)
-#   catalog[i] = CatalogStar(starid, s.RAJ2000, s.DEJ2000, s.f_mag)
-# end
-#
-# spd = generatespd(camera, catalog)
-
-
-using JLD2
-using FITSIO
-
-# Simulated image
-camera = Camera(2048, 2048, 5e-6, 100e-3)
-
-# @load "SKY2000_Magnitude6_doublestars_0.12_spd.jd2" spd
-
-f = CSV.file("test\\SKY2000_Magnitude6_doublestars_0.12.txt")
-
-catalog = Array{CatalogStar}(undef, f.rows)
-for (i, s) in enumerate(f)
-    catalog[i] = CatalogStar(s.ID, s.RAJ2000, s.DEJ2000, s.mag)
-end
-spd = generatespd(camera, catalog)
-
-
-f = FITS("test\\axy_m42.fits")
-X = read(f[2], "X")
-Y = read(f[2], "Y")
-
-imagestars = [SVector(x, y) for (x, y) in zip(X, Y)]
-
-
-
-imagecenter = SVector(camera.img_height/2, camera.img_width/2)
-
-# Determine star closest to center. This will be `starO`
-mindist = typemax(T)
-Oidx = 0
-for (i, star) in enumerate(imagestars)
-    # Vector from star to image center
-    svec = star - imagecenter
-
-    global mindist
-    global Oidx
-
-    # Distance from star to image center
-    d = norm(svec)
-
-    # Check if this star is closer to center than other stars
-    if d < mindist
-        mindist = d
-        Oidx = i
-    end
-end
-starO = imagestars[Oidx]
-
-# Calculate vectors from each star to `starO`
-neighbors = Array{UnknownStar}(undef, length(imagestars)-1)
-ni = 1
-for (i, star) in enumerate(imagestars)
-    global ni
-    if star != starO
-        svec = star - starO
-        d = norm(svec)
-        usvec = svec/d
-        neighbors[ni] = UnknownStar(i, SVector(star), svec, usvec, d)
-        ni += 1
-    # else starOid = i  # just fyi
-    end
-end
-sort!(neighbors, by = x -> x.d)
-
-# Check for distance matches with SPD
-candidateindices = Tuple{Int,Int}[]
-for (j, entry) in enumerate(spd)
-    for i in 1:N_NEAREST
-        if norm(entry.d - neighbors[i].d) < distancetolerance
-            push!(candidateindices, (i, j))
-        end
-    end
-end
-
-neighborpaths = Array{Array{SVector{2, Float64}}}(undef, 4)
-for i in 1:N_NEAREST
-    V1 = neighbors[i].vec
-    V1u = neighbors[i].uvec
-
-    sn = @view neighbors[i:end]
-    neighborpaths[i] = buildpath(sn, V1, V1u)
-end
-
-votecounts = vote(candidateindices, neighborpaths, spd)
-
-winnerid = argmax(votecounts)
-winnervotecount = votecounts[winnerid]
-winnervotecount > VOTE_THRESHOLD && println("Not enough votes!")
-
-winner = spd[candidateindices[winnerid][2]]
-
-
-
-
 
 end # module
